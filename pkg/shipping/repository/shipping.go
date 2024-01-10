b70943c2eb8f7e4a20ec0dff0113862e21c0129f
Leonardo Bispo <leonardobispo1000@gmail.com>
2024-01-10 11:42:10 -0300

    refactor: removed deadcode

--- pkg/shipping/repository/shipping.go
+++
@@ -1,9 +0,0 @@
-package repository
-
-type CarrierDBModel struct {
-	ID       string  `json:"id"`
-	Name     string  `json:"name"`
-	Service  string  `json:"service"`
-	Deadline int64   `json:"deadline"`
-	Price    float64 `json:"price"`
-}
